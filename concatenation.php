0f822ace646a89843a37bbeaaf6c3e1872b13ce1
Rabiya <rabiya@gmail.com>
2025-10-10 07:32:47 -0700

    first commit

---
+++ concatenation.php
@@ -0,0 +1,51 @@
+<?php
+$first = "Hello";
+$second = "World";
+
+// echo"". $first . "". $second ."";
+echo "<h1>". $first.$second."</h1>";
+?>
+
+
+<h1><?php echo $first . $second; ?></h1>
+
+
+
+<?php
+$num1 = 10;
+$num2 = 5;
+
+echo $num1-$num2;
+?> 
+
+<br>
+
+<?php
+$num1 =10;
+$num2 =30;
+echo $num1*$num2
+?>
+
+<br>
+
+<?php
+$num1 =10;
+$num2 =30;
+echo $num1+$num2
+?>
+
+<br>
+
+<?php
+$num1 =10;
+$num2 =30;
+echo $num1/$num2
+?>
+
+<br>
+
+<?php
+$num1 =10;
+$num2 =30;
+echo $num1%$num2
+?>
